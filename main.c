4721ddf944a0d33e1f6dbeb76f86cc7f9579a6a4
arybin <arybin@163.com>
2017-04-26 23:39:51 +0800

    added 3 methods

--- main.c
+++ main.c
@@ -6,9 +6,7 @@ typedef struct {
   int size;
 }IntArray, *PIntArray;
 
-typedef void (*IntArraySorter)(PIntArray);
-//random integers in range [1,max]
-
+typedef PIntArray (*IntArraySorter)(PIntArray);
 PIntArray buildIntArray(int size){
   PIntArray pIntArray;
   pIntArray=(PIntArray)malloc(sizeof(IntArray));
@@ -69,19 +67,81 @@ void releaseIntArray(PIntArray pIntArray){
   free(pIntArray);
 }
 
-void sortIntArrayBy(PIntArray pIntArray,IntArraySorter sorter){
-  sorter(pIntArray);
+PIntArray sortIntArrayBy(PIntArray pIntArray,IntArraySorter sorter){
+  return sorter(pIntArray);
 }
+//typedef void (*IntArraySorter)(PIntArray);
+void linkSort(PIntArray pIntArray){
+  printf("%d\n",pIntArray->size);
+  pIntArray->data[0]=1234;
+}
+
+PIntArray bubbleSort(PIntArray pOldArray){
+  PIntArray pNewArray=cloneIntArray(pOldArray);
+  int i,j;
+  for(i=0;i<pNewArray->size;i++){
+    for(j=0;j<pNewArray->size-i-1;j++){
+      if(pNewArray->data[j+1]<pNewArray->data[j]){
+        pNewArray->data[j]^=pNewArray->data[j+1];
+        pNewArray->data[j+1]^=pNewArray->data[j];
+        pNewArray->data[j]^=pNewArray->data[j+1];
+      }
+    }
+  }
+  return pNewArray;
+}
+
+PIntArray normalInsertSort(PIntArray pOldArray){
+  PIntArray pNewArray=buildIntArray(pOldArray->size);
+  int i,j;
+  int *tmp=(int*)malloc(pOldArray->size*sizeof(int)+1);
+  for(i=1;i<pOldArray->size+1;i++){
+    tmp[i]=pOldArray->data[i-1];
+  }
+  for(i=1;i<pOldArray->size+1;i++){
+    tmp[0]=tmp[i];
+    j=i;
+    while(tmp[j-1]>tmp[0]){
+      tmp[j]=tmp[j-1];
+      j--;
+    }
+    tmp[j]=tmp[0];
+  }
+  for(i=0;i<pOldArray->size;i++){
+    pNewArray->data[i]=tmp[i+1];
+  }
+  return pNewArray;
+}
+
+PIntArray binInsertSort(PIntArray pOldArray){
+  PIntArray pNewArray=cloneIntArray(pOldArray);
+  int i,j,low,middle,high,tmp;
+  for(i=1;i<pNewArray->size;i++){
+    tmp=pNewArray->data[i];
+    low=0;high=i-1;
+    if (tmp<pNewArray->data[i-1]){
+      while(low<=high){
+        middle=(low+high)/2;
+        if(pNewArray->data[middle]<=tmp){
+          low=middle+1;
+          continue;
+        }
+        high=middle-1;
+      }
+      for(j=i;j>low;j--){
+        pNewArray->data[j]=pNewArray->data[j-1];
+      }
+      pNewArray->data[low]=tmp;
+    }
+  }
+  return pNewArray;
+}
+
 int main(){
-
-  PIntArray pIntArrayA = randomIntArray(10,15,10);
-  printlnIntArray(pIntArrayA);
-  PIntArray pIntArrayB = cloneIntArray(pIntArrayA);
-  printlnIntArray(pIntArrayB);
-  pIntArrayB->data[0]=99;
-  printlnIntArray(pIntArrayA);
-  printlnIntArray(pIntArrayB);
-  
+  PIntArray pIntArray = randomIntArray(1,30,10);
+  printlnIntArray(pIntArray);
+  printlnIntArray(sortIntArrayBy(pIntArray,binInsertSort));
+  printlnIntArray(sortIntArrayBy(pIntArray,normalInsertSort));
+  printlnIntArray(sortIntArrayBy(pIntArray,bubbleSort));
   return 0;
-
 }
